2155267319c5687d2621ae86f295d0d974a32872
张为杰 <zhangweijie@cestc.cn>
2022-10-08 11:00:22 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    post: 3456,
+  },
+});
