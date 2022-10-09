2ddd70974b81352151569b5b7ef39394c4713af3
张为杰 <zhangweijie@cestc.cn>
2022-10-09 11:24:29 +0800

    有个歌报错;

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -11,7 +11,7 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/country?country_code=CN' }),
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
     getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode || 'CN'}` }),
     getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
